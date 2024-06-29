a4698a5a8e9ce38a516fe07fdabad17b337f7c20
Serhat <115489292+serhatlyildiz@users.noreply.github.com>
2024-06-29 06:59:35 +0300

    SpringBoot ile OBS işlemi
    SpringBoot ile Öğrenci CRUD işlemleri

---
+++ obs/obs/src/main/java/edu/obs/obs/ObsApplication.java
@@ -0,0 +1,13 @@
+package edu.obs.obs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObsApplication.class, args);
+	}
+
+}
